320d24098e34cadb630bf8379441b9f2091ca855
czam20 <ccza2010@gmail.com>
2021-09-25 22:46:54 -0400

    Update parse_news

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@ import lxml.html as html
 
 HOME_URL = 'https://www.larepublica.co/'
 
-XPATH_LINK_TO_ARTICLE = '//h2/a/@href'
-XPATH_TITLE = '//div[@class="mb-auto"]/h2/span/text()'
+XPATH_LINK_TO_ARTICLE = '//text-fill/a/@href'
+XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_ARTICLE = '//div[@class="html-content"]/p/text()'
 
@@ -16,13 +16,24 @@ def parse_news(link, date):
         if response.status_code == 200:
             article_page = response.content.decode('utf-8')
             parsed = html.fromstring(article_page)
-            title = parsed.xpath(XPATH_TITLE)
-            summary = parsed.xpath(XPATH_SUMMARY)
-            article = parsed.xpath(XPATH_ARTICLE)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"', '')
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                article = parsed.xpath(XPATH_ARTICLE)
+            except IndexError:
+                return
             
-            print(title)
-            # print(summary)
-            # print(article)
+            with open(f'{date}/{title}.txt', 'w', encoding='utf-8') as file:
+                file.write(title)
+                file.write('\n\n')
+                file.write(summary)
+                file.write('\n\n')
+                
+                for p in article:
+                    file.write(p)
+                    file.write('\n')
         else:
             raise ValueError(f'Error {response.status_code}')
     except ValueError as e:
@@ -37,12 +48,13 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             link_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-          
             today = datetime.date.today().strftime('%m-%d-%Y')
             if not os.path.isdir(today):
                 #Se crea la carpeta de la fecha actual
                 os.mkdir(today) 
-            
+
+            print(len(link_to_notices))
+
             for link in link_to_notices:
                 parse_news(link, today)
         else:
@@ -55,4 +67,3 @@ def run():
     
 if __name__ == '__main__':
     run()
-    
